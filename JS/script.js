18b63836d9cacef1d137f1d79a9177c3ad4d0fde
Melissa Martinez <melmar12@live.com>
2015-06-10 19:34:25 -0500

    added animation to thumbnails in module 2 also changed h1 style in module 1 and removed tiled background image on video

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,16 @@
+$(window).scroll(function(){
+    
+    var wScroll = $(this).scrollTop();
+    
+    if(wScroll > $('#Mod1').offset().top + 400) {
 
-  
-  console.log('hiii'); 
-  
+    $('#woman-box div').each(function(i){
+
+      setTimeout(function(){
+        $('#woman-box div').eq(i).addClass('is-showing');
+      }, (700 * (Math.exp(i * 0.14))) - 700);
+    });
+
+  }
+    
+});
