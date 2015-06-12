1f04cdde1fddd70b43c7ad17c4e56c669cbdbfdc
Melissa Martinez <melmar12@live.com>
2015-06-12 02:03:11 -0500

    removed scrolling animation, menu links now work

--- JS/script.js
+++ JS/script.js
@@ -1,19 +1,26 @@
-$( "#content-box" ).load( "woman.html" );
+$( "#content-box" ).load( "inserts/woman.html" );
 
 
-$(window).scroll(function(){
+
+// menu clicks
+// add loading animation?
+$("#man-link").click(function() {
     
-    var wScroll = $(this).scrollTop();
+    $( "#content-box" ).load( "inserts/man.html" );
     
-    if(wScroll > $('#Mod1').offset().top + 400) {
+    return false;
+});
 
-    $('#content-box div').each(function(i){
-
-      setTimeout(function(){
-        $('#content-box div').eq(i).addClass('is-showing');
-      }, (700 * (Math.exp(i * 0.14))) - 700);
-    });
-
-  }
+$("#woman-link").click(function() {
     
+    $( "#content-box" ).load( "inserts/woman.html" );
+         
+    return false;
+});
+
+$("#storelocator-link").click(function() {
+    
+    $( "#content-box" ).load( "inserts/storelocator.html" );
+    
+    return false;
 });
