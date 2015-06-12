fe35a67a0e6df083bfc348b032f1f919c0fe33a1
Melissa Martinez <melmar12@live.com>
2015-06-11 23:44:13 -0500

    changed some CSS Ids

--- JS/script.js
+++ JS/script.js
@@ -4,10 +4,10 @@ $(window).scroll(function(){
     
     if(wScroll > $('#Mod1').offset().top + 400) {
 
-    $('#woman-box div').each(function(i){
+    $('#content-box div').each(function(i){
 
       setTimeout(function(){
-        $('#woman-box div').eq(i).addClass('is-showing');
+        $('#content-box div').eq(i).addClass('is-showing');
       }, (700 * (Math.exp(i * 0.14))) - 700);
     });
 
